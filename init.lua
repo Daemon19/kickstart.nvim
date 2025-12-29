653508712e946b8c41122728b55b85dd84e4ca85
Daemon19 <komanglangendria04@gmail.com>
2025-12-29 20:37:01 +0700

    🏃 Add mini.move plugin

--- init.lua
+++ init.lua
@@ -939,6 +939,32 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      require('mini.move').setup(
+        -- No need to copy this inside `setup()`. Will be used automatically.
+        {
+          -- Module mappings. Use `''` (empty string) to disable one.
+          mappings = {
+            -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+            left = '<M-h>',
+            right = '<M-l>',
+            down = '<M-j>',
+            up = '<M-k>',
+
+            -- Move current line in Normal mode
+            line_left = '<M-h>',
+            line_right = '<M-l>',
+            line_down = '<M-j>',
+            line_up = '<M-k>',
+          },
+
+          -- Options which control moving behavior
+          options = {
+            -- Automatically reindent selection during linewise vertical move
+            reindent_linewise = true,
+          },
+        }
+      )
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
